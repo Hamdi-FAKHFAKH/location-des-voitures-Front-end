9c99cce6cecc41cc7908c2ba413bb5045046ab26
hamdi <hamdi.fakhfakh98@gmail.com>
2022-04-11 13:28:12 +0100

    erreur

--- src/components/Body/Listeofcar.js
+++ src/components/Body/Listeofcar.js
@@ -1,10 +1,35 @@
 
-import React from 'react'
+import React, { useState } from 'react'
 import { Container, Row } from 'reactstrap'
 import {liste} from '../../assets/data/listcar'
 import CartVoitureUp from './CartVoitureUp'
 
 export default function Listeofcar() {
+
+  const [ voiture, setVoitures ] = useState({});
+
+  const getVoitures = () => {
+    const requestOptions = {
+        method: 'Get',
+        headers: { 
+          // 'Authorization': Json.string
+          'Content-Type': 'application/json', 
+          'Accept': 'application/json'
+        }
+    };
+
+    fetch('http://localhost:3000/api/voiture/mesVoitures', requestOptions)
+    .then(response => response.json())
+    .then(data => {
+        setVoitures(data)
+        // store.dispatch( signIn() )
+        // console.log("store: ",store.getState())
+        // setToken(data.token);
+        // setUserId(data.clientId);
+    })
+    .catch(err => console.error(err));
+    }
+
   return (
     <div>
          <Container style={{marginTop:'40px',margin:'auto',maxWidth:'900px' , marginLeft:'60px'}}>
